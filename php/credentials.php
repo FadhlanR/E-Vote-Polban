7ffea027bc3a79f794908e62424ea54a3e392ff7
waffi <waffifaturrahman@yahoo.co.id>
2016-11-26 16:03:48 +0700

    Update credentials.php

--- php/credentials.php
+++ php/credentials.php
@@ -6,5 +6,5 @@ define("DB_PASS", "");
 define("DB_NAME", "pemira_db");
 define("DB_DSN", "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME);
 
-define("ADMIN_PASSWORD", ''); // #BirukanKemaPolban
+define("ADMIN_PASSWORD", 'birukankemapolban'); // #BirukanKemaPolban
 ?>
