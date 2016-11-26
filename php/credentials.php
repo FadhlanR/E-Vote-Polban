503d69f3489e79d9db530d4d6215d2d61f309cab
waffi <waffifaturrahman@yahoo.co.id>
2016-11-26 16:02:06 +0700

    Update credentials.php

--- php/credentials.php
+++ php/credentials.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "db_pemira");
+define("DB_NAME", "pemira_db");
 define("DB_DSN", "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME);
 
 define("ADMIN_PASSWORD", ''); // #BirukanKemaPolban
